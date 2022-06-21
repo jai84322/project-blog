a500e822fe3188c1d0696482853115b76207f9b8
dhirajuranium <dhirajpatil2311@gmail.com>
2022-06-21 17:04:04 +0530

    1APicomplete

---
+++ src/controllers/authorController.js
@@ -0,0 +1,48 @@
+const authorModel = require('../models/authorModel')
+
+
+const createAuthor = async function (req, res) {
+    let data = req.body;
+    let { fname, lname, title, email, password } = req.body
+    if(Object.keys(data).length == 0){
+        return res.status(400).send({ status: false, msg: "Please enter data in the request" })
+
+    }
+
+
+    if (!fname) {
+        return res.status(400).send({ status: false, msg: "fname is missing" })
+    }
+
+    if (!lname) {
+        return res.status(400).send({ status: false, msg: "lname is missing" })
+    }
+
+    if (!title) {
+        return res.status(400).send({ status: false, msg: "title is missing" })
+    }
+
+    if (!email) {
+        return res.status(400).send({ status: false, msg: "email is missing" })
+    }
+
+    if (!password) {
+        return res.status(400).send({ status: false, msg: "password is missing" })
+    }
+
+    let uniqueEmail = await authorModel.findOne({email:email}) 
+    
+    if(uniqueEmail){
+        return res.status(400).send({ status: false, msg: "This email is already exist" })
+
+    }
+
+
+
+    let savedData = await authorModel.create(data)
+    res.status(201).send({ status: true, data: savedData })
+
+
+}
+
+module.exports.createAuthor = createAuthor
---
+++ src/routes/route.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const router = express.Router()
+const authorController = require('../controllers/authorController')
+
+
+
+
+
+
+
+router.post('/authors',authorController.createAuthor)
+
+module.exports=router
