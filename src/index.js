fc157075dab6ee09b30f0b121296a6490968c067
jai84322 <jai84322@gmail.com>
2022-06-21 14:13:19 +0530

    model-done

--- src/index.js
+++ src/index.js
@@ -1,21 +1,43 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const {default :mongoose} = require('mongoose');
+const route = require('./routes/route.js');
+const app =express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+mongoose.connect("mongodb+srv://jai84322:Bing%401234%23@demo.3li78.mongodb.net/project1-blog?retryWrites=true&w=majority"
+,{useNewUrlParser: true})
+.then(()=>console.log('MongoDB is connected'))
+.catch(err => console.log(err));
+
+app.use('/',route);
+app.listen(process.env.PORT||3000,function(){
+    console.log('express app running on PORT'+(process.env.PORT||3000))
+});
+
+
 // const express = require('express');
 // const bodyParser = require('body-parser');
-// const {default :mongoose} = require('mongoose');
-// const app =express();
+// const route = require('./routes/route.js');
+// const { default: mongoose } = require('mongoose');
+// const app = express();
 
-// const route = require('./routes/route');
 // app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({extended: true}));
+// app.use(bodyParser.urlencoded({ extended: true }));
 
 
+// mongoose.connect("mongodb+srv://jai84322:Bing%401234%23@demo.3li78.mongodb.net/project1-blog?retryWrites=true&w=majority", {
+//     useNewUrlParser: true
+// })
+// .then( () => console.log("MongoDb is connected"))
+// .catch ( err => console.log(err) )
 
-// mongoose.connect("mongodb+srv://dhirajpatil:XuEAzywgRheQB7B1@cluster0.0v32f.mongodb.net/Dhiraj2198-DB?retryWrites=true&w=majority"
-// ,{useNewUrlParser: true});
+// app.use('/', route)
 
-// .then(()=>console.log('MongoDB is connected'));
-// .catch(err => console.log(err));
 
-// app.use('/',route);
-// app.listen(process.env.PORT||3000,function(){
-//     console.log('express app running on PORT'+(process.env.PORT||3000))
+// app.listen(process.env.PORT || 3000, function () {
+//     console.log('Express app running on port ' + (process.env.PORT || 3000))
 // });
---
+++ src/models/blogModel.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose')
+const ObjectId = mongoose.Schema.Types.ObjectId
+
+const blogSchema = new mongoose.Schema({
+    
+    title : {
+        type : String,
+        required : true,
+        trim : true
+    },
+    body : {
+        type : String,
+        required : true,
+        trim : true
+    },
+    authorId : {
+        type : ObjectId,
+        ref : "author",
+        required: true
+    },
+    tags : [String],
+    category: {
+        type : String,
+        required: true,
+        trim : true
+    },
+    susubcategory : [String],
+    deletedAt : {
+        type : Date
+    },
+    isDeleted : {
+        type : Boolean,
+        default: false
+    },
+    publishedAt : {
+        type : Date
+    },
+    isPublished: {
+        type : Boolean,
+        default: false
+    }
+
+
+
+}, {timestamps : true})
+
+module.exports = mongoose.model("blog", blogSchema)
