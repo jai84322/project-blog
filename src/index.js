615f5d2e801d42e2f92dfdd2c1caebf370843e70
WASIF738871 <101876913+WASIF738871@users.noreply.github.com>
2022-06-21 13:41:49 +0530

    wasif

---
+++ src/index.js
@@ -0,0 +1,21 @@
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const {default :mongoose} = require('mongoose');
+// const app =express();
+
+// const route = require('./routes/route');
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended: true}));
+
+
+
+// mongoose.connect("mongodb+srv://dhirajpatil:XuEAzywgRheQB7B1@cluster0.0v32f.mongodb.net/Dhiraj2198-DB?retryWrites=true&w=majority"
+// ,{useNewUrlParser: true});
+
+// .then(()=>console.log('MongoDB is connected'));
+// .catch(err => console.log(err));
+
+// app.use('/',route);
+// app.listen(process.env.PORT||3000,function(){
+//     console.log('express app running on PORT'+(process.env.PORT||3000))
+// });
